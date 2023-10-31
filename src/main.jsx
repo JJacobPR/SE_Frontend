3eaf2ea13732e7bdd9b22f308d1179c64d6170dd
Jakub Janicki <jjanicki08@gmail.com>
2023-10-31 18:12:56 +0100

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomeView from "./views/HomeView.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.scss";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeView />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

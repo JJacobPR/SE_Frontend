ff5efbebf756d3c2236c73579f818e2497e37738
pxgal <pablogalvanadan@gmail.com>
2023-11-26 20:30:51 +0100

    login correct

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -4,22 +4,6 @@ import Navbar from "../components/navbar/Navbar";
 import axios from 'axios';
 
 class HomeView extends Component {
-  async componentDidMount() {
-    try {
-      // Make a GET request to "/api/csrf-cookie"
-      const response = await axios.get('/api/csrf-cookie');
-
-      const xsrfToken = response.headers['set-cookie'];
-
-      console.log('XSRF Token:', xsrfToken);
-
-    } catch (error) {
-      // Handle errors if the request fails
-      console.error('Error setting CSRF Cookie:', error);
-    }
-  }
-  
-
   render() {
     return (
       <>

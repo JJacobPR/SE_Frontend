6fcbede8eb17ef11a71c509a389f6e094405c0f7
Jakub Janicki <jjanicki08@gmail.com>
2023-11-28 14:21:10 +0100

    Refactoring code to modern standars

--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -1,47 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "../styles/LoginView.scss";
 import "../index.scss";
 import superhero from "../assets/img/eco-man.svg";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-class LoginView extends React.Component {
-  render() {
-    return (
-      <div className="LoginSite">
-        <div>
-          <img className="superHero" src={superhero}/>
+const LoginView = () => {
+  return (
+    <div className="LoginSite">
+      <div>
+        <img className="superHero" src={superhero} />
+      </div>
+      <div className="LoginMain">
+        <h2>
+          We need our superhero! <FontAwesomeIcon icon={faRightToBracket} beat />
+        </h2>
+        <form>
+          <div className="LoginMail">
+            <label>
+              Email
+              <input type="text" name="mail" autoComplete="on" />
+            </label>
+          </div>
+
+          <div className="LoginPass">
+            <label>
+              Password
+              <input type="password" name="password" autoComplete="on" />
+            </label>
+          </div>
+          <button className="button1">
+            Sign in <FontAwesomeIcon icon={faRightToBracket} />
+          </button>
+        </form>
+
+        <div className="ForgotPassHref">
+          <a href="/">
+            Forgot Password? <FontAwesomeIcon icon={faEnvelope} />
+          </a>
         </div>
-                <div className="LoginMain">
-                    <h2>We need our superhero! <FontAwesomeIcon icon={faRightToBracket} beat/></h2>
-                    <form>
-                        <div className="LoginMail">
-                            <label>Email
-                            <input type="text" name="mail" autoComplete="on" />
-                            </label>
-                        </div>
-                        
-                        <div className="LoginPass">
-                            <label>Password
-                            <input type="password" name="password" autoComplete="on" />
-                            </label>
-                        </div>
-                        <button className="button1" >Sign in <FontAwesomeIcon icon={faRightToBracket} /></button>
-                    </form>
-                    
-                    <div className="ForgotPassHref">
-                        <a href="/" >Forgot Password? <FontAwesomeIcon icon={faEnvelope} /></a>
-                    </div>
-                    
-                    <div className="RegisterHref">
-                        <a href="/register">Sign up <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
-                    </div>
-                </div>
-            </div>
-    );
-  }
-}
+
+        <div className="RegisterHref">
+          <a href="/register">
+            Sign up <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LoginView;
--- src/views/ProfileView.jsx
+++ src/views/ProfileView.jsx
@@ -1,14 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-class ProfileView extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <p>Profile</p>
-      </div>
-    );
-  }
-}
+const ProfileView = (props) => {
+  return (
+    <div>
+      <p>Profile</p>
+    </div>
+  );
+};
 
 export default ProfileView;

c273c560565e8753b6168d19a7e6a2b8b7dc2663
Mateusz Kubiak <matkubiak888@gmail.com>
2023-12-07 18:43:11 +0100

    splash arts

--- src/views/NpcView.jsx
+++ src/views/NpcView.jsx
@@ -1,9 +1,18 @@
+import { SvgIcon } from '@mui/material';
 import React, { useState } from 'react';
+
+import greenleaf from "../assets/img/npc/greenleaf.png";
+import redbird from "../assets/img/npc/redbird.png";
+import splashspeed from "../assets/img/npc/splashspeed.png";
 
 class NpcView extends React.Component {
     render() {
         return(
-            <div>Amogus</div>
+            <div>
+                <img src={greenleaf}></img>
+                <img src={redbird}></img>
+                <img src={splashspeed}></img>
+            </div>
         );
     }
 }

95c3a530402f0777e78563c7275b3877be127962
Mateusz Kubiak <matkubiak888@gmail.com>
2023-12-16 22:05:07 +0100

    feat: normal looking yet fully local notifications

--- src/components/Notifications/Notifications.jsx
+++ src/components/Notifications/Notifications.jsx
@@ -1,51 +1,69 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import styles from "./Notifications.module.scss";
-import { Button, List, ListItem } from "@mui/material";
-// import DeleteIcon from '@mui/icons-material/Delete';
+import { Button, List, Divider } from "@mui/material";
 
-function NotificationHolder() {
+function Notifications(props) {
+	const getNotifications = () => {
+		// here use axios to get notifications from database
+		return [
+			{'id': 123, 'header': 'Message', 'body':'User RickAstley sent you a message.'},
+			{'id': 987, 'header': 'Quiz', 'body':'You can try quizes again!'},
+			{'id': 760, 'header': 'Friend Request', 'body':'User Goku wants to be your friend!'}
+		  ]
+	  };
+
+	const [notifications, setNotifications] = useState(getNotifications());
+	console.log(notifications);
+
+	const clearAllNotifications = () => {
+		if (notifications.length === 0) {
+			console.log('No notifications to clear!');
+			return;
+		}
+
+		// here use axios to clear all notifications
+
+		setNotifications([]);
+		console.log('Cleared!');
+		console.log(notifications);
+	};
     
-    const loadNotifications = () => {
-        // here use axios to get notifications from database
-        return [
-            {'header': 'aa?', 'body':'AAAA'},
-            {'header': 'bb?', 'body':'BBB'}
-        ]
-    };
+	const clearNotification = (index) => {
+		// here use axios to clear a single notification
 
-    // load notifications here
-    const notifications = loadNotifications();
+		const notif_uuid = notifications[index]['id']
 
-    const clearAllNotifications = () => {
-        // here use axios to delete all notifications
-        console.log('Cleared!');
-        console.log(notifications);
-    };
-    
-    const clearNotification = (index) => {
-        console.log('Cleared index: ' + index)
-    };
+		const updatedNotifications = [...notifications];
+		updatedNotifications.splice(index, 1);
+		setNotifications(updatedNotifications);
 
-    return (
-        <div id='main'>
-          <div id='header'>
-            Notifications
-            <button onClick={clearAllNotifications}>Clear All</button>
-          </div>
+		console.log('Cleared notificaiton no: ' + index + ' and uuid: ' + notif_uuid)
+		console.log(notifications);
+	};
 
-        <div >
-          <List id='body' className={styles["notif-list"]}>
-              {notifications.map((item, index) => (
-                <ListItem key="{index}" className={styles["notif-item"]}>
-                    <p className={styles["notif-header"]}>{item.header}</p>
-                    <p>{item.body}</p>
-                    <Button onClick={() => clearNotification(index)}>x</Button>
-                </ListItem>
-              ))}
-          </List>
-          </div>
-      </div>
-    );
+	return (
+	<div className={styles["all"]}>
+		<div className={styles["header"]}>
+			<p>Notifications</p>
+			{ notifications.length !== 0 && <button className={styles["all-clear-button"]} onClick={clearAllNotifications}>Clear</button>}
+		</div>
+		<div>
+			<List id='body' className={styles["list"]}>
+				{ notifications.length === 0 ? <p className={styles["no-new-notifs"]}>No new notifications</p> :
+				notifications.map((item, index) => (
+					<ul key={index} className={styles["item"]}>
+						<div className={styles["item-header"]}>
+							<p>{item.header}</p>
+							<button className={styles["item-clear-button"]} onClick={() => clearNotification(index)}>x</button>
+						</div>
+						<p className={styles["item-body"]}>{item.body}</p>
+							<Divider variant="middle" />
+					</ul>
+				))}
+			</List>
+        </div>
+    </div>
+  );
 }
 
-export default NotificationHolder
+export default Notifications
